fd40c11fabd54671795d02573cf5b224b420c42c
Adriana Burgos <kymsar.lol@gmail.com>
2023-07-24 19:44:38 +0200

    Fourth test deployment

--- server/server.js
+++ server/server.js
@@ -20,6 +20,6 @@ if (process.env.NODE_ENV === 'production') {
   }
 
 const PORT = process.env.PORT || 5000;
-const host = process.env.HOST || '0.0.0.0';
+//const HOST = process.env.HOST || '0.0.0.0';
 
 app.listen(PORT, () => console.log(`Server started port ${PORT}`));
