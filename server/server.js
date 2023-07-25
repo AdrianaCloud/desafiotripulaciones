86f154890303092a1f30038e7ad03b4ac76f5d20
Adriana Burgos <kymsar.lol@gmail.com>
2023-07-25 13:40:42 +0200

    Changes to server.js

--- server/server.js
+++ server/server.js
@@ -14,9 +14,9 @@ app.get("/test", (req, res) => {
 //* Serve static assets in production, must be at this location of this file
 if (process.env.NODE_ENV === 'production') {
     //*Set static folder
-    app.use(express.static('client/build'));
+    app.use(express.static(__dirname, '..', 'client', 'build'));
     
-    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html')));
   }
 
 const PORT = process.env.PORT || 5000;
