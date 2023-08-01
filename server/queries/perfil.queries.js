533a2387dce0e9adb553db7e1f4d1d7daef8a637
Alvaro3c <alvaro.cuallado.g@gmail.com>
2023-08-01 20:04:14 +0200

    clear of conflict

--- server/queries/perfil.queries.js
+++ server/queries/perfil.queries.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const perfilQueries = {
     getPerfilByEmail: `SELECT * FROM ${process.env.PERFIL_TABLE} WHERE user_id = @user_id`,
     getPerfiles: `SELECT * FROM ${process.env.PERFIL_TABLE}`,
-    createPerfil: `INSERT INTO ${process.env.PERFIL_TABLE} (perfil_id, user_id, preferencias_deportivas, tipo_de_dieta, objetivo_entrenamiento, ciudad, edad, sexo, peso, condicion, altura) VALUES (@perfil_id, @ciudad, @user_id, @preferencias_deportivas, @tipo_de_dieta, @ciudad, @objetivo_entrenamiento, @edad, @sexo, @peso, @condicion, @altura)`,
+    createPerfil: `INSERT INTO ${process.env.PERFIL_TABLE} (perfil_id, ciudad, user_id, preferencias_deportivas, tipo_de_dieta, objetivo_entrenamiento, edad, sexo, peso, condicion, altura) VALUES (@perfil_id, @ciudad, @user_id, @preferencias_deportivas, @tipo_de_dieta, @objetivo_entrenamiento, @edad, @sexo, @peso, @condicion, @altura)`,
     updatePerfil: `UPDATE ${process.env.PERFIL_TABLE} SET preferencias_deportivas = @preferencias_deportivas, tipo_de_dieta = @tipo_de_dieta, objetivo_entrenamiento = @objetivo_entrenamiento, edad = @edad, sexo = @sexo, peso = @peso, condicion = @condicion, altura = @altura  WHERE user_id = @user_id`
 }
 
